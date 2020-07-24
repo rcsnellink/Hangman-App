53f5fea9cf200e37fad378ab118517fd4c9244d8
Ramon Snellink <r.snellink@gmail.com>
2020-07-24 18:09:55 +0200

    starting with http req

--- app.js
+++ app.js
@@ -16,8 +16,24 @@ const game1 = new Hangman("Cat Dus", 3);
 renderPuzzle();
 renderRemaining();
 
-window.addEventListener("keypress", function (e) {
+window.addEventListener("keypress", (e) => {
   game1.makeGuess(e.key);
   renderPuzzle();
   renderRemaining();
 });
+
+// making an http request
+
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText);
+    console.log(data);
+  } else if (e.target.readyState === 4) {
+    console.log("An error has taken place");
+  }
+});
+
+request.open("GET", "http://puzzle.mead.io/puzzle?la=la");
+request.send();
