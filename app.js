33fe7d8060f33cc3b2effd45f885ed18e51ab869
Ramon Snellink <r.snellink@gmail.com>
2020-07-29 20:29:24 +0200

    converted to fetch. more practice with APIs

--- app.js
+++ app.js
@@ -22,30 +22,27 @@ window.addEventListener("keypress", (e) => {
   renderRemaining();
 });
 
-//Hij roept de functie getPuzzle aan, met een functie als argument.
-//Deze functie heeft 2 parameters, error en puzzle.
-
-getPuzzle("2").then(
-  (puzzle) => {
+getPuzzle("2")
+  .then((puzzle) => {
     console.log(puzzle);
-  },
-  (error) => {
-    console.log(`Error: ${error}`);
-  }
-);
+  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  });
 
-getCountry("NL").then(
-  (country) => {
-    console.log(country.name);
-  },
-  (error) => {
-    console.log(`The error: ${error}}`);
-  }
-);
+// getCountry("NL")
+//   .then((country) => {
+//     console.log(country.name);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-// create function for getting country details
-// call it with 2 arguments, country code, callback function
-// make the http req and callback  with country info
-// use the callback to print the country name
-//
-// making an http request
+getLocation()
+  .then((location) => {
+    return getCountry(location.country);
+  })
+  .then((country) => console.log(country.name))
+  .catch((err) => {
+    console.log(err);
+  });
--- requests.js
+++ requests.js
@@ -1,38 +1,33 @@
-//Als getPuzzle wordt aangeroepen dan doet hij een HTTP req.
-// Als readystate 4 is, en target status = 200, dan assigned hij de data.
-// Daarna roept hij callback, wat eigenlijk dus de functie is die ik invoerde als argument bij getPuzzle.
-// Als target status niet 200 is dan doet hij een andere callback, met een error message.
-
-const getPuzzle = (wordCount) =>
-  new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener("readystatechange", (e) => {
-      if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        resolve(data.puzzle);
-      } else if (e.target.readyState === 4) {
-        reject("An error has taken place");
+const getPuzzle = (wordCount) => {
+  return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Unable to fetch a new puzzle");
       }
-    });
-    request.open("GET", `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
-    request.send();
-  });
+    })
+    .then((data) => data.puzzle); // return niet het hele object maar alleen de puzzle waarde.
+};
 
-const getCountry = (countryCode) =>
-  new Promise((resolve, reject) => {
-    const countryRequest = new XMLHttpRequest();
-    countryRequest.open("GET", "http://restcountries.eu/rest/v2/all");
-    countryRequest.send();
-
-    countryRequest.addEventListener("readystatechange", (e) => {
-      if (e.target.readyState === 4 && e.target.status === 200) {
-        const countries = JSON.parse(e.target.responseText);
-        const country = countries.find((country) => country.alpha2Code === countryCode);
-        resolve(country);
-        // callback
-      } else if (e.target.readyState === 4) {
-        reject("An error has occured");
+const getCountry = (countryCode) => {
+  return fetch("http://restcountries.eu/rest/v2/all")
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Unable to fetch data");
       }
-    });
+    })
+    .then((countries) => countries.find((country) => country.alpha2Code === countryCode)); //return een individueel country object
+};
+
+const getLocation = () => {
+  return fetch("http://ipinfo.io/json?token=e6fa78ce09d685").then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error("Unable to fetch data");
+    }
   });
+};
